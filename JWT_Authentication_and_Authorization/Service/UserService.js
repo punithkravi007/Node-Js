df0f8f557df626e02a8cdc5e90ddaebd05c92a30
Punith KR <68544398+punithkravi007@users.noreply.github.com>
2021-01-27 09:57:16 +0530

    Jwt Auth Sample

---
+++ JWT_Authentication_and_Authorization/Service/UserService.js
@@ -0,0 +1,15 @@
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+];
+
+module.exports = {
+    users
+}
---
+++ JWT_Authentication_and_Authorization/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const { users } = require("./Service/UserService");
+const accessTokenSecret = "youraccesstokensecret";
+const authenticateJWT = require("./Service/authenticationService");
+const app = express();
+
+app.use(bodyParser.json());
+app.listen(3000, () => {
+  console.log("Authentication service started on port 3000");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Fetch the User and Role from Database against the credentials user passed and store it in user variable.
+
+  const user = users.find((u) => {
+    return u.username === username && u.password === password;
+  });
+
+  
+
+  if (user) {
+    const accessToken = jwt.sign(
+      { username: user.username, role: user.role },
+      accessTokenSecret
+    );
+
+    res.json({
+      accessToken,
+    });
+  } else {
+    res.send("Username or password incorrect");
+  }
+});
+
+app.get("/getUser", authenticateJWT, (req, res) => {
+  res.send("User Fetch is Success");
+});
+
+module.exports = app;
